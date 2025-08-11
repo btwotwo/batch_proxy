011202140773beb2e4870d94a42cc45fa8e23f9d
btwotwo <jocoding@tuta.com>
2025-08-11 12:50:16 +0200

    Rename config to settings, add settings

--- src/config.rs
+++
@@ -1,4 +0,0 @@
-pub struct BatchConfiguration {
-    pub max_batch_size: usize,
-    pub max_waiting_time_ms: u64,
-}
---
+++ src/settings.rs
@@ -0,0 +1,45 @@
+use std::env;
+
+use config::{Config, ConfigError, Environment, File};
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug, Clone)]
+#[allow(unused)]
+pub struct BatchSettings {
+    pub max_batch_size: usize,
+    pub max_waiting_time_ms: u64,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[allow(unused)]
+pub struct ApiSettings {
+    pub target_port: u16,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[allow(unused)]
+pub struct InferenceApiSettings {
+    pub target_url: String,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[allow(unused)]
+pub struct Settings {
+    pub api: ApiSettings,
+    pub inference_api: InferenceApiSettings,
+    pub batch: BatchSettings,
+}
+
+impl Settings {
+    pub fn new() -> Result<Self, ConfigError> {
+        let run_mode = env::var("RUN_MODE").unwrap_or_else(|_| "development".into());
+        let s = Config::builder()
+            .add_source(File::with_name("settings"))
+            .add_source(File::with_name(&format!("settings.{run_mode}")).required(false))
+            .add_source(File::with_name("settings.local").required(false))
+            .add_source(Environment::with_prefix("batch_proxy"))
+            .build()?;
+
+        s.try_deserialize()
+    }
+}
